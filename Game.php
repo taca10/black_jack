7d770a358a1f29017b69b59e9444c6f47c5e5220
taca10 <taka.ur.0505@gmail.com>
2019-03-23 17:52:09 +0900

    Gameクラスから不要な関数削除

--- Game.php
+++ Game.php
@@ -40,43 +40,6 @@ class Game
         }
     }
 
-    function convertToScore($messages) {
-        for ($i=0; $i<count($messages); $i++) {
-          $points = $this->convertFaceToNumber($messages, $i);
-          if ($i < 2){
-            $this->userPoints += intval(preg_replace('/[^0-9]/', '', $messages[$i]));
-            $this->userPoints += $points;
-          }else {
-            $this->dealerPoints += intval(preg_replace('/[^0-9]/', '', $messages[$i]));
-            $this->dealerPoints += $points;
-          }
-          if (count($messages) == 1) {
-            echo "あなたの現在の得点は" . $this->userPoints . "です\n";
-            return;
-          }
-        }
-        if(preg_replace('/[^0-9]/', '', $this->dealerCard[0]) == "") {
-          $this->dealerPoints += 10;
-        }else {
-          $this->dealerPoints += intval(preg_replace('/[^0-9]/', '', $this->dealerCard[0]));
-        }
-        echo "あなたの現在の得点は" . $this->userPoints . "です\n";
-    }
-
-    function convertFaceToNumber($messages, $i) {
-        $points = 0;
-        if(strpos($messages[$i], 'J')) {
-          $points += 10;
-        }elseif(strpos($messages[$i], 'Q')) {
-          $points += 10;
-        }elseif(strpos($messages[$i], 'K')) {
-          $points += 10;
-        }elseif(strpos($messages[$i], 'A')) {
-          $points += 1;
-        }
-        return $points;
-    }
-
     public function nextStart($card) {
         echo "カードを引きますか？引く場合はYを引かない場合はNを入力してください\n";
         $stdin = trim(fgets(STDIN));
